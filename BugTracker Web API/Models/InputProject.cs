ac559da4338c0a55c297c939158fe5f711b1428e
Rakesh-syren <133647067+Rakesh-syren@users.noreply.github.com>
2023-06-28 15:25:36 +0530

    Update InputProject.cs

--- BugTracker Web API/Models/InputProject.cs
+++ BugTracker Web API/Models/InputProject.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Models
         public string ProjectName { get; set; } = null!;
 
         [Required]
-        [RegularExpression("^[a-zA-Z0-9- _]$")]
+        [RegularExpression("^[a-zA-Z0-9- _]{1,}$")]
 
         public string ProjectId { get; set; } = null!;
     }
